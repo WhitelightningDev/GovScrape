7eda4d8d74437327d6937dd5f2139c61b8f20925
whitelightningdev <danielmommsen@hotmail.com>
2024-12-06 15:20:32 +0200

    Fixed the Frame inport issue

--- Govscraper.py
+++ Govscraper.py
@@ -6,7 +6,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.frame import Frame
 from webdriver_manager.chrome import ChromeDriverManager
 
 
